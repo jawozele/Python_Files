61deb447d63f4f275334ee92e9f7e6215905d622
Josh Awozele <35323325+jawozele@users.noreply.github.com>
2019-02-11 18:59:16 +0000

    Add files via upload

---
+++ Lists.py
@@ -0,0 +1,29 @@
+# A program that stores a List of cars. The output is displayed with all cars saved on
+
+cars = ['Mercedes Benz',
+        'Toyota',
+        'BMW',
+        'Hyundai',
+        'Mitsubishi',
+        'Land Rover',
+        'Audi',
+        'Ford Focus']
+
+#print(cars[0])
+
+
+#print(cars[1:4])
+
+#print(len(cars))
+
+cars.sort()
+
+
+cars.append('Chrysler')
+
+cars.remove('Hyundai')
+
+
+
+
+print (cars)
