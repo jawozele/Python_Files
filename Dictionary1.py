18ace047e85c32824e83027b51ba62009ff7b7ae
Josh Awozele <35323325+jawozele@users.noreply.github.com>
2019-02-11 19:00:43 +0000

    Add files via upload

---
+++ Dictionary1.py
@@ -0,0 +1,28 @@
+MLB_team = {
+    'Colorado'  :   'Rockies',
+    'Boston'    :   'Red Sox',
+    'Minnesota' :   'Twins',
+    'Milwaukee' :   'Brewers',
+    'Seattle'   :   'Mariners'
+}
+
+print(MLB_team)
+
+MLB_team = {
+
+
+    'Colorado'  :   'Rockies',
+    'Boston'    :   'Red Sox',
+    'Minnesota' :   'Twins',
+    'Milwaukee' :   'Brewers',
+    'Seattle'   :   'Mariners'
+
+
+
+
+}
+
+'Colorado' in MLB_team
+'Boston' in MLB_team
+'Minnesota' in MLB_team
+
